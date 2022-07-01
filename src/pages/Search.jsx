d20ea6c681b7aa323fcbd65ac13570c78354ecde
felipe <fortes.felipe95@gmail.com>
2022-07-01 18:58:21 -0300

    Se tudo certo, este commit serve para criar alguns dados do meu componente Search e validar o botão caso meu input contenha dois ou mais caracteres

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,13 +2,38 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      artistNameInput: '',
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      artistNameInput: event.target.value,
+    });
+  }
+
   render() {
+    const { artistNameInput } = this.state;
     return (
       <div
         data-testid="page-search"
       >
         <Header />
-        Search
+        <input
+          data-testid="search-artist-input"
+          placeholder="Nome do artista"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ artistNameInput.length < 2 }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
