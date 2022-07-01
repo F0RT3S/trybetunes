a23fec6ae280a818ddd1756efb5ccb98ce655e9f
felipe <fortes.felipe95@gmail.com>
2022-07-01 15:54:06 -0300

    Se tudo certo, este commit faz a requisição na função e redireciona o usuário para a página Search

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
       userName: '',
+      loading: false,
+      redirect: false,
     };
   }
 
@@ -14,22 +19,42 @@ class Home extends Component {
     });
   };
 
-  render() {
+  saveName = async () => {
     const { userName } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: userName });
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
+  }
+
+  render() {
+    const { userName, loading, redirect } = this.state;
     const TRES = 3;
     return (
       <div data-testid="page-login">
-        <input
-          data-testid="login-name-input"
-          onChange={ this.handleChange }
-        />
-        <button
-          data-testid="login-submit-button"
-          type="button"
-          disabled={ userName.length < TRES }
-        >
-          Entrar
-        </button>
+        {
+          loading ? <Loading /> : (
+            <form>
+              <input
+                data-testid="login-name-input"
+                onChange={ this.handleChange }
+              />
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ userName.length < TRES }
+                onClick={ this.saveName }
+              >
+                Entrar
+              </button>
+            </form>
+          )
+        }
+        { redirect && <Redirect to="/search" /> }
       </div>
     );
   }
