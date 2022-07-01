701ce258c9827fafaca944b9b70a487901c6b223
felipe <fortes.felipe95@gmail.com>
2022-07-01 15:06:21 -0300

    Se tudo certo, este commit valida o input para habilitar e desabilitar o botão caso a quantidade de caracteres seja menor que 3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      userName: event.target.value,
+    });
+  };
+
   render() {
+    const { userName } = this.state;
+    const TRES = 3;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <input
+          data-testid="login-name-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ userName.length < TRES }
+        >
+          Entrar
+        </button>
+      </div>
     );
   }
 }
