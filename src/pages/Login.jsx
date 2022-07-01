48b4013f3f6208021e6e08abdbe0ff2f77a859df
felipe <fortes.felipe95@gmail.com>
2022-07-01 14:42:11 -0300

    Se tudo certo, este commit cria as rotas necessárias para navegar entre meus componentes criados

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div data-testid="page-login">Login</div>
+    );
+  }
+}
+
+export default Home;
