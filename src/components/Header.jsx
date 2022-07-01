650d9b6441e1a5877c71cc551bfd535b4d12623e
felipe <fortes.felipe95@gmail.com>
2022-07-01 18:14:46 -0300

    Se tudo certo, este commit exibe o componente Loading enquanto espera a API e retorna o nome do  usuário apos o carregamento

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
+import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      name: '',
+    };
+  }
+
   componentDidMount() {
-    getUser();
+    this.loadingGetUser(); // Como não posso colocar async no didMount, tive que criar outra função (Mentoria Estruturada Gabs)
+  }
+
+  loadingGetUser = async () => {
+    this.setState({
+      loading: true,
+    });
+    const response = await getUser();
+    // console.log(response);
+    this.setState({
+      loading: false,
+      name: response.name,
+    });
   }
 
   render() {
+    const { loading, name } = this.state;
     return (
       <header
         data-testid="header-component"
       >
-        Header
+        {
+          loading ? <Loading /> : (
+            <p data-testid="header-user-name">
+              Bem vindo(a),
+              {' '}
+              {name}
+            </p>
+          )
+        }
       </header>
     );
   }
